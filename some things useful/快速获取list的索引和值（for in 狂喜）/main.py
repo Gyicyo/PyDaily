fdcbf0789f7e9499607366a65cd53ec749d4fb6c
Gyicyo <1831041410@qq.com>
2024-03-30 20:17:47 +0800

    Probably the most useful feature

---
+++ some things useful/快速获取list的索引和值（for in 狂喜）/main.py
@@ -0,0 +1,9 @@
+"""
+    Create At 2024/3/30
+    At:20:16 PM
+"""
+
+l: list[str] = ['a', 'b', 'c', 'd','e','f']
+
+for index,value in enumerate(l,start=0): # start为起始index，默认为0
+    print(index,value)
