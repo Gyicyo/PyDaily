89bab2c66c0c090cef661eb68eb28fc98527b2bc
Gyicyo <1831041410@qq.com>
2024-03-30 14:23:09 +0800

    is not java

---
+++ some things useful/string/main.py
@@ -0,0 +1,16 @@
+"""
+    Create At 2024/3/30
+    At:14:21 PM
+    get all the string constants in python from string module
+"""
+import string
+
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
+print(string.whitespace)
+print(string.printable)
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.hexdigits)
+print(string.octdigits)
